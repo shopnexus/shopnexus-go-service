1fd06a57539b9b33713017d64ce685cccbc767fd
Đậu Khoa <khoakomlem@gmail.com>
2025-02-27 00:37:35 +0700

    update errors

--- internal/model/error.go
+++ internal/model/error.go
@@ -3,8 +3,14 @@ package model
 import "errors"
 
 var (
-	ErrForbidden     = errors.New("forbidden")
-	ErrTokenInvalid  = errors.New("token invalid")
-	ErrInvalidCreds  = errors.New("invalid credentials")
+	ErrForbidden       = errors.New("forbidden")
+	ErrMalformedParams = errors.New("malformed request parameters")
+	ErrTokenInvalid    = errors.New("token invalid")
+	ErrInvalidCreds    = errors.New("invalid credentials")
+
+	//Account
 	ErrWrongPassword = errors.New("wrong password")
+
+	// Refund
+	// ErrRefund
 )
