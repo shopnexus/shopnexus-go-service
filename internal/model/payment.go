80a2d165aa328ece25e5f918b96a1d351db77ff9
Đậu Khoa <khoakomlem@gmail.com>
2025-02-27 00:37:51 +0700

    add refund model

--- internal/model/payment.go
+++ internal/model/payment.go
@@ -1,25 +1,29 @@
 package model
 
 type (
-	PaymentStatus string
+	Status        string
 	PaymentMethod string
+	RefundMethod  string
 )
 
 const (
-	PaymentStatusPending   PaymentStatus = "PENDING"
-	PaymentStatusSuccess   PaymentStatus = "SUCCESS"
-	PaymentStatusCancelled PaymentStatus = "CANCELLED"
-	PaymentStatusFailed    PaymentStatus = "FAILED"
+	StatusPending   Status = "PENDING"
+	StatusSuccess   Status = "SUCCESS"
+	StatusCancelled Status = "CANCELLED"
+	StatusFailed    Status = "FAILED"
 
-	PaymentMethodCash     PaymentMethod = "CASH"
-	PaymentMethodBankMomo PaymentMethod = "MOMO"
-	PaymentMethodVnpay    PaymentMethod = "VNPAY"
+	PaymentMethodCash  PaymentMethod = "CASH"
+	PaymentMethodMomo  PaymentMethod = "MOMO"
+	PaymentMethodVnpay PaymentMethod = "VNPAY"
+
+	RefundMethodDropOff RefundMethod = "DROP_OFF"
+	RefundMethodPickUp  RefundMethod = "PICK_UP"
 )
 
 type ProductOnPayment struct {
 	ItemQuantityBase[string]
-	Price      int64 `json:"price"`
-	TotalPrice int64 `json:"total_price"`
+	Price      int64 `json:"price"`       // Single price, maybe have discount
+	TotalPrice int64 `json:"total_price"` // Total price, maybe have discount if reach certain quantity
 }
 
 type Payment struct {
@@ -28,8 +32,21 @@ type Payment struct {
 	Address       string        `json:"address"`
 	PaymentMethod PaymentMethod `json:"payment_method"`
 	Total         int64         `json:"total"`
-	Status        PaymentStatus `json:"status"`
+	Status        Status        `json:"status"`
 	DateCreated   int64         `json:"date_created"`
 
 	Products []ProductOnPayment `json:"products"`
+}
+
+type Refund struct {
+	ID          int64        `json:"id"` /* unique */
+	PaymentID   int64        `json:"payment_id"`
+	Method      RefundMethod `json:"method"`
+	Status      Status       `json:"status"`
+	Reason      string       `json:"reason"`
+	Address     *string      `json:"address"`
+	DateCreated int64        `json:"date_created"`
+	DateUpdated int64        `json:"date_updated"`
+
+	Resources []string `json:"resources"`
 }
