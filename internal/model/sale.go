3b96c3d96462b8dbfc76792359cc4ee0c5699fd2
Đậu Khoa <khoakomlem@gmail.com>
2025-03-24 19:12:26 +0700

    update sale logic, add max discount

--- internal/model/sale.go
+++ internal/model/sale.go
@@ -1,15 +1,36 @@
 package model
 
 type Sale struct {
-	ID              int64   `json:"id"` /* unique */
-	Tag             *string `json:"tag"`
-	ProductModelID  *int64  `json:"product_model_id"`
-	BrandID         *int64  `json:"brand_id"`
-	DateStarted     int64   `json:"date_started"`
-	DateEnded       *int64  `json:"date_ended"`
-	Quantity        int64   `json:"quantity"`
-	Used            int64   `json:"used"`
-	IsActive        bool    `json:"is_active"`
-	DiscountPercent *int32  `json:"discount_percent"`
-	DiscountPrice   *int64  `json:"discount_price"`
+	ID               int64   `json:"id"` /* unique */
+	Tag              *string `json:"tag"`
+	ProductModelID   *int64  `json:"product_model_id"`
+	BrandID          *int64  `json:"brand_id"`
+	DateCreated      int64   `json:"date_created"`
+	DateStarted      int64   `json:"date_started"`
+	DateEnded        *int64  `json:"date_ended"`
+	Quantity         int64   `json:"quantity"`
+	Used             int64   `json:"used"`
+	IsActive         bool    `json:"is_active"`
+	DiscountPercent  *int32  `json:"discount_percent"`
+	DiscountPrice    *int64  `json:"discount_price"`
+	MaxDiscountPrice int64   `json:"max_discount_price"`
+}
+
+// Apply calculates the final sale discount
+func (s Sale) Apply(price int64) int64 {
+	var discount int64
+
+	if s.DiscountPercent != nil {
+		discount = price - price*int64(*s.DiscountPercent)/100
+	}
+
+	if s.DiscountPrice != nil {
+		discount = *s.DiscountPrice
+	}
+
+	if s.MaxDiscountPrice > 0 && discount > s.MaxDiscountPrice {
+		discount = s.MaxDiscountPrice
+	}
+
+	return discount
 }
--- internal/repository/sale.go
+++ internal/repository/sale.go
@@ -18,18 +18,58 @@ func (r *Repository) GetSale(ctx context.Context, id int64) (model.Sale, error) 
 	}
 
 	return model.Sale{
-		ID:              row.ID,
-		Tag:             pgxutil.PgtypeToPtr[string](row.Tag),
-		ProductModelID:  pgxutil.PgtypeToPtr[int64](row.ProductModelID),
-		BrandID:         pgxutil.PgtypeToPtr[int64](row.BrandID),
-		DateStarted:     row.DateStarted.Time.UnixMilli(),
-		DateEnded:       util.PtrTimeToMilis(pgxutil.PgtypeToPtr[time.Time](row.DateEnded)),
-		Quantity:        row.Quantity,
-		Used:            row.Used,
-		IsActive:        row.IsActive,
-		DiscountPercent: pgxutil.PgtypeToPtr[int32](row.DiscountPercent),
-		DiscountPrice:   pgxutil.PgtypeToPtr[int64](row.DiscountPrice),
+		ID:               row.ID,
+		Tag:              pgxutil.PgtypeToPtr[string](row.Tag),
+		ProductModelID:   pgxutil.PgtypeToPtr[int64](row.ProductModelID),
+		BrandID:          pgxutil.PgtypeToPtr[int64](row.BrandID),
+		DateCreated:      row.DateCreated.Time.UnixMilli(),
+		DateStarted:      row.DateStarted.Time.UnixMilli(),
+		DateEnded:        util.PtrTimeToMilis(pgxutil.PgtypeToPtr[time.Time](row.DateEnded)),
+		Quantity:         row.Quantity,
+		Used:             row.Used,
+		IsActive:         row.IsActive,
+		DiscountPercent:  pgxutil.PgtypeToPtr[int32](row.DiscountPercent),
+		DiscountPrice:    pgxutil.PgtypeToPtr[int64](row.DiscountPrice),
+		MaxDiscountPrice: row.MaxDiscountPrice,
 	}, nil
+}
+
+type GetLatestSaleParams struct {
+	ProductModelID int64
+	BrandID        int64
+	Tags           []string
+}
+
+func (r *Repository) GetAvailableSales(ctx context.Context, params GetLatestSaleParams) ([]model.Sale, error) {
+	rows, err := r.sqlc.GetAvailableSales(ctx, sqlc.GetAvailableSalesParams{
+		ProductModelID: params.ProductModelID,
+		BrandID:        params.BrandID,
+		Tags:           params.Tags,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	sales := make([]model.Sale, 0, len(rows))
+	for _, row := range rows {
+		sales = append(sales, model.Sale{
+			ID:               row.ID,
+			Tag:              pgxutil.PgtypeToPtr[string](row.Tag),
+			ProductModelID:   pgxutil.PgtypeToPtr[int64](row.ProductModelID),
+			BrandID:          pgxutil.PgtypeToPtr[int64](row.BrandID),
+			DateCreated:      row.DateCreated.Time.UnixMilli(),
+			DateStarted:      row.DateStarted.Time.UnixMilli(),
+			DateEnded:        util.PtrTimeToMilis(pgxutil.PgtypeToPtr[time.Time](row.DateEnded)),
+			Quantity:         row.Quantity,
+			Used:             row.Used,
+			IsActive:         row.IsActive,
+			DiscountPercent:  pgxutil.PgtypeToPtr[int32](row.DiscountPercent),
+			DiscountPrice:    pgxutil.PgtypeToPtr[int64](row.DiscountPrice),
+			MaxDiscountPrice: row.MaxDiscountPrice,
+		})
+	}
+
+	return sales, nil
 }
 
 type ListSalesParams struct {
@@ -77,17 +117,19 @@ func (r *Repository) ListSales(ctx context.Context, params ListSalesParams) ([]m
 	sales := make([]model.Sale, 0, len(rows))
 	for _, row := range rows {
 		sales = append(sales, model.Sale{
-			ID:              row.ID,
-			Tag:             pgxutil.PgtypeToPtr[string](row.Tag),
-			ProductModelID:  pgxutil.PgtypeToPtr[int64](row.ProductModelID),
-			BrandID:         pgxutil.PgtypeToPtr[int64](row.BrandID),
-			DateStarted:     row.DateStarted.Time.UnixMilli(),
-			DateEnded:       util.PtrTimeToMilis(pgxutil.PgtypeToPtr[time.Time](row.DateEnded)),
-			Quantity:        row.Quantity,
-			Used:            row.Used,
-			IsActive:        row.IsActive,
-			DiscountPercent: pgxutil.PgtypeToPtr[int32](row.DiscountPercent),
-			DiscountPrice:   pgxutil.PgtypeToPtr[int64](row.DiscountPrice),
+			ID:               row.ID,
+			Tag:              pgxutil.PgtypeToPtr[string](row.Tag),
+			ProductModelID:   pgxutil.PgtypeToPtr[int64](row.ProductModelID),
+			BrandID:          pgxutil.PgtypeToPtr[int64](row.BrandID),
+			DateCreated:      row.DateCreated.Time.UnixMilli(),
+			DateStarted:      row.DateStarted.Time.UnixMilli(),
+			DateEnded:        util.PtrTimeToMilis(pgxutil.PgtypeToPtr[time.Time](row.DateEnded)),
+			Quantity:         row.Quantity,
+			Used:             row.Used,
+			IsActive:         row.IsActive,
+			DiscountPercent:  pgxutil.PgtypeToPtr[int32](row.DiscountPercent),
+			DiscountPrice:    pgxutil.PgtypeToPtr[int64](row.DiscountPrice),
+			MaxDiscountPrice: row.MaxDiscountPrice,
 		})
 	}
 
@@ -96,63 +138,68 @@ func (r *Repository) ListSales(ctx context.Context, params ListSalesParams) ([]m
 
 func (r *Repository) CreateSale(ctx context.Context, sale model.Sale) (model.Sale, error) {
 	row, err := r.sqlc.CreateSale(ctx, sqlc.CreateSaleParams{
-		Tag:             *pgxutil.PtrToPgtype(&pgtype.Text{}, sale.Tag),
-		ProductModelID:  *pgxutil.PtrToPgtype(&pgtype.Int8{}, &sale.ProductModelID),
-		BrandID:         *pgxutil.PtrToPgtype(&pgtype.Int8{}, &sale.BrandID),
-		DateStarted:     *pgxutil.ValueToPgtype(&pgtype.Timestamptz{}, time.UnixMilli(sale.DateStarted)),
-		DateEnded:       *pgxutil.PtrToPgtype(&pgtype.Timestamptz{}, util.PtrMilisToTime(sale.DateEnded)),
-		Quantity:        sale.Quantity,
-		Used:            sale.Used,
-		IsActive:        sale.IsActive,
-		DiscountPercent: pgtype.Int4{Int32: util.DerefDefault(sale.DiscountPercent, 0), Valid: sale.DiscountPercent != nil},
-		DiscountPrice:   *pgxutil.PtrToPgtype(&pgtype.Int8{}, sale.DiscountPrice),
+		Tag:              *pgxutil.PtrToPgtype(&pgtype.Text{}, sale.Tag),
+		ProductModelID:   *pgxutil.PtrToPgtype(&pgtype.Int8{}, &sale.ProductModelID),
+		BrandID:          *pgxutil.PtrToPgtype(&pgtype.Int8{}, &sale.BrandID),
+		DateStarted:      *pgxutil.ValueToPgtype(&pgtype.Timestamptz{}, time.UnixMilli(sale.DateStarted)),
+		DateEnded:        *pgxutil.PtrToPgtype(&pgtype.Timestamptz{}, util.PtrMilisToTime(sale.DateEnded)),
+		Quantity:         sale.Quantity,
+		Used:             sale.Used,
+		IsActive:         sale.IsActive,
+		DiscountPercent:  pgtype.Int4{Int32: util.DerefDefault(sale.DiscountPercent, 0), Valid: sale.DiscountPercent != nil},
+		DiscountPrice:    *pgxutil.PtrToPgtype(&pgtype.Int8{}, sale.DiscountPrice),
+		MaxDiscountPrice: sale.MaxDiscountPrice,
 	})
 	if err != nil {
 		return model.Sale{}, err
 	}
 
 	return model.Sale{
-		ID:              row.ID,
-		Tag:             pgxutil.PgtypeToPtr[string](row.Tag),
-		ProductModelID:  pgxutil.PgtypeToPtr[int64](row.ProductModelID),
-		BrandID:         pgxutil.PgtypeToPtr[int64](row.BrandID),
-		DateStarted:     row.DateStarted.Time.UnixMilli(),
-		DateEnded:       util.PtrTimeToMilis(pgxutil.PgtypeToPtr[time.Time](row.DateEnded)),
-		Quantity:        row.Quantity,
-		Used:            row.Used,
-		IsActive:        row.IsActive,
-		DiscountPercent: pgxutil.PgtypeToPtr[int32](row.DiscountPercent),
-		DiscountPrice:   pgxutil.PgtypeToPtr[int64](row.DiscountPrice),
+		ID:               row.ID,
+		Tag:              pgxutil.PgtypeToPtr[string](row.Tag),
+		ProductModelID:   pgxutil.PgtypeToPtr[int64](row.ProductModelID),
+		BrandID:          pgxutil.PgtypeToPtr[int64](row.BrandID),
+		DateCreated:      row.DateCreated.Time.UnixMilli(),
+		DateStarted:      row.DateStarted.Time.UnixMilli(),
+		DateEnded:        util.PtrTimeToMilis(pgxutil.PgtypeToPtr[time.Time](row.DateEnded)),
+		Quantity:         row.Quantity,
+		Used:             row.Used,
+		IsActive:         row.IsActive,
+		DiscountPercent:  pgxutil.PgtypeToPtr[int32](row.DiscountPercent),
+		DiscountPrice:    pgxutil.PgtypeToPtr[int64](row.DiscountPrice),
+		MaxDiscountPrice: row.MaxDiscountPrice,
 	}, nil
 }
 
 type UpdateSaleParams struct {
-	ID              int64
-	Tag             *string
-	ProductModelID  *int64
-	BrandID         *int64
-	DateStarted     *int64
-	DateEnded       *int64
-	Quantity        *int64
-	Used            *int64
-	IsActive        *bool
-	DiscountPercent *int32
-	DiscountPrice   *int64
+	ID               int64
+	Tag              *string
+	ProductModelID   *int64
+	BrandID          *int64
+	DateStarted      *int64
+	DateEnded        *int64
+	Quantity         *int64
+	Used             *int64
+	IsActive         *bool
+	DiscountPercent  *int32
+	DiscountPrice    *int64
+	MaxDiscountPrice *int64
 }
 
 func (r *Repository) UpdateSale(ctx context.Context, params UpdateSaleParams) error {
 	return r.sqlc.UpdateSale(ctx, sqlc.UpdateSaleParams{
-		ID:              params.ID,
-		Tag:             *pgxutil.PtrToPgtype(&pgtype.Text{}, params.Tag),
-		ProductModelID:  *pgxutil.PtrToPgtype(&pgtype.Int8{}, params.ProductModelID),
-		BrandID:         *pgxutil.PtrToPgtype(&pgtype.Int8{}, params.BrandID),
-		DateStarted:     *pgxutil.PtrToPgtype(&pgtype.Timestamptz{}, util.PtrMilisToTime(params.DateStarted)),
-		DateEnded:       *pgxutil.PtrToPgtype(&pgtype.Timestamptz{}, util.PtrMilisToTime(params.DateEnded)),
-		Quantity:        *pgxutil.PtrToPgtype(&pgtype.Int8{}, params.Quantity),
-		Used:            *pgxutil.PtrToPgtype(&pgtype.Int8{}, params.Used),
-		IsActive:        *pgxutil.PtrToPgtype(&pgtype.Bool{}, params.IsActive),
-		DiscountPercent: pgtype.Int4{Int32: util.DerefDefault(params.DiscountPercent, 0), Valid: params.DiscountPercent != nil},
-		DiscountPrice:   *pgxutil.PtrToPgtype(&pgtype.Int8{}, params.DiscountPrice),
+		ID:               params.ID,
+		Tag:              *pgxutil.PtrToPgtype(&pgtype.Text{}, params.Tag),
+		ProductModelID:   *pgxutil.PtrToPgtype(&pgtype.Int8{}, params.ProductModelID),
+		BrandID:          *pgxutil.PtrToPgtype(&pgtype.Int8{}, params.BrandID),
+		DateStarted:      *pgxutil.PtrToPgtype(&pgtype.Timestamptz{}, util.PtrMilisToTime(params.DateStarted)),
+		DateEnded:        *pgxutil.PtrToPgtype(&pgtype.Timestamptz{}, util.PtrMilisToTime(params.DateEnded)),
+		Quantity:         *pgxutil.PtrToPgtype(&pgtype.Int8{}, params.Quantity),
+		Used:             *pgxutil.PtrToPgtype(&pgtype.Int8{}, params.Used),
+		IsActive:         *pgxutil.PtrToPgtype(&pgtype.Bool{}, params.IsActive),
+		DiscountPercent:  pgtype.Int4{Int32: util.DerefDefault(params.DiscountPercent, 0), Valid: params.DiscountPercent != nil},
+		DiscountPrice:    *pgxutil.PtrToPgtype(&pgtype.Int8{}, params.DiscountPrice),
+		MaxDiscountPrice: *pgxutil.PtrToPgtype(&pgtype.Int8{}, params.MaxDiscountPrice),
 	})
 }
 
