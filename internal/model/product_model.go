358515efff4f4c0b98e70a911d39ab35270ace59
Đậu Khoa <khoakomlem@gmail.com>
2025-03-24 19:12:57 +0700

    add model id

--- internal/model/product_model.go
+++ internal/model/product_model.go
@@ -2,6 +2,7 @@ package model
 
 type ProductModel struct {
 	ID               int64  `json:"id"` /* unique */
+	Type             int64  `json:"type"`
 	BrandID          int64  `json:"brand_id"`
 	Name             string `json:"name"`
 	Description      string `json:"description"`
