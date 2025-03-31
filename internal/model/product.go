4c679adf864d3723af3535a460663ace2a374aab
Đậu Khoa <khoakomlem@gmail.com>
2025-04-01 00:42:24 +0700

    refactor product metadata to use json.RawMessage

--- internal/model/product.go
+++ internal/model/product.go
@@ -1,25 +1,24 @@
 package model
 
+import "encoding/json"
+
 // ProductIdentifier is a struct to identify a product, either by ID or SerialID
 type ProductIdentifier struct {
 	ID       *int64
 	SerialID *string
 }
 
-type Product[T any] struct {
-	ID             int64  `json:"id"`        /* unique */
-	SerialID       string `json:"serial_id"` /* unique */
-	Quantity       int64  `json:"quantity,omitempty"`
-	Sold           int64  `json:"sold,omitempty"`
-	AddPrice       int64  `json:"add_price,omitempty"`
-	IsActive       bool   `json:"is_active"`
-	ProductModelID int64  `json:"product_model_id"`
-	Metadata       T      `json:"metadata"`
-	DateCreated    int64  `json:"date_created"`
-	DateUpdated    int64  `json:"date_updated"`
-}
+type Product struct {
+	ID             int64           `json:"id"`        /* unique */
+	SerialID       string          `json:"serial_id"` /* unique */
+	Quantity       int64           `json:"quantity,omitempty"`
+	Sold           int64           `json:"sold,omitempty"`
+	AddPrice       int64           `json:"add_price,omitempty"`
+	IsActive       bool            `json:"is_active"`
+	ProductModelID int64           `json:"product_model_id"`
+	Metadata       json.RawMessage `json:"metadata"`
+	DateCreated    int64           `json:"date_created"`
+	DateUpdated    int64           `json:"date_updated"`
 
-type ProductMetadataShoe struct {
-	Size  int    `json:"size"`
-	Color string `json:"color"` // hex color code
+	Resources []string `json:"resources"`
 }
