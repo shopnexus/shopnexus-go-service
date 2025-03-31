84e8fd74bceb7242c2c9cc6942adb6b87347ec7a
Đậu Khoa <khoakomlem@gmail.com>
2025-04-01 00:28:24 +0700

    add get account info

--- internal/grpc/handler/account/account.go
+++ internal/grpc/handler/account/account.go
@@ -2,6 +2,7 @@ package account
 
 import (
 	"context"
+	"shopnexus-go-service/internal/grpc/interceptor/auth"
 	"shopnexus-go-service/internal/model"
 	"shopnexus-go-service/internal/service/account"
 
@@ -17,6 +18,35 @@ type ImplementedAccountServiceHandler struct {
 
 func NewAccountServiceHandler(service *account.AccountService) accountv1connect.AccountServiceHandler {
 	return &ImplementedAccountServiceHandler{service: service}
+}
+
+func (s *ImplementedAccountServiceHandler) GetUser(ctx context.Context, req *connect.Request[accountv1.GetUserRequest]) (*connect.Response[accountv1.GetUserResponse], error) {
+	claims, err := auth.GetAccount(req)
+	if err != nil {
+		return nil, err
+	}
+
+	account, err := s.service.FindAccount(ctx, account.FindAccountParams{
+		Role:   model.RoleUser,
+		UserID: &claims.UserID,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	user, ok := account.(model.AccountUser)
+	if !ok {
+		return nil, model.ErrForbidden
+	}
+
+	return connect.NewResponse(&accountv1.GetUserResponse{
+		Email:            user.Username,
+		Phone:            user.Phone,
+		Username:         user.Username,
+		Gender:           convertGenderToProto(user.Gender),
+		FullName:         user.FullName,
+		DefaultAddressId: user.DefaultAddressID,
+	}), nil
 }
 
 func (s *ImplementedAccountServiceHandler) LoginUser(ctx context.Context, req *connect.Request[accountv1.LoginUserRequest]) (*connect.Response[accountv1.LoginUserResponse], error) {
@@ -101,5 +131,18 @@ func convertGender(protoGender accountv1.Gender) model.Gender {
 		panic("gender is unspecified")
 	default:
 		panic("unknown gender")
+	}
+}
+
+func convertGenderToProto(gender model.Gender) accountv1.Gender {
+	switch gender {
+	case model.GenderMale:
+		return accountv1.Gender_GENDER_MALE
+	case model.GenderFemale:
+		return accountv1.Gender_GENDER_FEMALE
+	case model.GenderOther:
+		return accountv1.Gender_GENDER_OTHER
+	default:
+		return accountv1.Gender_GENDER_UNSPECIFIED
 	}
 }
