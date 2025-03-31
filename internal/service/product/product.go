3e59b1867c3e0ec20075965fb75308537b766096
Đậu Khoa <khoakomlem@gmail.com>
2025-04-01 00:35:10 +0700

    move to new file

--- internal/service/product/product.go
+++ internal/service/product/product.go
@@ -4,13 +4,14 @@ import (
 	"context"
 	"shopnexus-go-service/internal/model"
 	"shopnexus-go-service/internal/repository"
-	"shopnexus-go-service/internal/service/s3"
+	"shopnexus-go-service/internal/util"
 )
 
 type ProductService struct {
 	repo *repository.Repository
-	s3   *s3.S3Service
 }
+
+var _ ProductServiceInterface = (*ProductService)(nil)
 
 func NewProductService(repo *repository.Repository) *ProductService {
 	return &ProductService{
@@ -18,160 +19,13 @@ func NewProductService(repo *repository.Repository) *ProductService {
 	}
 }
 
-func (s *ProductService) GetBrand(ctx context.Context, id int64) (model.Brand, error) {
-	brand, err := s.repo.GetBrand(ctx, id)
-	if err != nil {
-		return model.Brand{}, err
-	}
-
-	return brand, nil
-}
-
-type ListBrandsParams = repository.ListBrandsParams
-
-func (s *ProductService) ListBrands(ctx context.Context, params ListBrandsParams) (result model.PaginateResult[model.Brand], err error) {
-	total, err := s.repo.CountBrands(ctx, params)
-	if err != nil {
-		return result, err
-	}
-
-	brands, err := s.repo.ListBrands(ctx, params)
-	if err != nil {
-		return result, err
-	}
-
-	return model.PaginateResult[model.Brand]{
-		Data:       brands,
-		Limit:      params.Limit,
-		Page:       params.Page,
-		Total:      total,
-		NextPage:   params.NextPage(total),
-		NextCursor: nil,
-	}, nil
-}
-
-type CreateBrandParams struct {
-	UserID int64
-	model.Brand
-}
-
-func (s *ProductService) CreateBrand(ctx context.Context, params CreateBrandParams) (model.Brand, error) {
-	txRepo, err := s.repo.Begin(ctx)
-	if err != nil {
-		return model.Brand{}, err
-	}
-	defer txRepo.Rollback(ctx)
-
-	newBrand, err := txRepo.CreateBrand(ctx, params.Brand)
-	if err != nil {
-		return model.Brand{}, err
-	}
-
-	if err := txRepo.Commit(ctx); err != nil {
-		return model.Brand{}, err
-	}
-
-	return newBrand, nil
-}
-
-type UpdateBrandParams struct {
-	repository.UpdateBrandParams
-}
-
-func (s *ProductService) UpdateBrand(ctx context.Context, params UpdateBrandParams) error {
-	// TODO: chuyển isAdmin này ra chỗ khác
-	// if isAdmin, err := s.Account.IsAdmin(ctx, params.UserID); err != nil {
-	// 	return err
-	// } else if !isAdmin {
-	// 	return model.ErrForbidden
-	// }
-
-	return s.repo.UpdateBrand(ctx, params.UpdateBrandParams)
-}
-
-func (s *ProductService) DeleteBrand(ctx context.Context, id int64) error {
-	return s.repo.DeleteBrand(ctx, id)
-}
-
-func (s *ProductService) GetProductModel(ctx context.Context, id int64) (model.ProductModel, error) {
-	productModel, err := s.repo.GetProductModel(ctx, id)
-	if err != nil {
-		return model.ProductModel{}, err
-	}
-
-	return productModel, nil
-}
-
-type ListProductModelsParams = repository.ListProductModelsParams
-
-func (s *ProductService) ListProductModels(ctx context.Context, params ListProductModelsParams) (result model.PaginateResult[model.ProductModel], err error) {
-	total, err := s.repo.CountProductModels(ctx, params)
-	if err != nil {
-		return result, err
-	}
-
-	productModels, err := s.repo.ListProductModels(ctx, params)
-	if err != nil {
-		return result, err
-	}
-
-	return model.PaginateResult[model.ProductModel]{
-		Data:       productModels,
-		Limit:      params.Limit,
-		Page:       params.Page,
-		Total:      total,
-		NextPage:   params.NextPage(total),
-		NextCursor: nil,
-	}, nil
-}
-
-type CreateProductModelParams struct {
-	UserID int64
-	model.ProductModel
-}
-
-func (s *ProductService) CreateProductModel(ctx context.Context, params CreateProductModelParams) (model.ProductModel, error) {
-	txRepo, err := s.repo.Begin(ctx)
-	if err != nil {
-		return model.ProductModel{}, err
-	}
-	defer txRepo.Rollback(ctx)
-
-	productModel, err := txRepo.CreateProductModel(ctx, params.ProductModel)
-	if err != nil {
-		return model.ProductModel{}, err
-	}
-
-	if err := txRepo.Commit(ctx); err != nil {
-		return model.ProductModel{}, err
-	}
-
-	return productModel, nil
-}
-
-type UpdateProductModelParams = repository.UpdateProductModelParams
-
-func (s *ProductService) UpdateProductModel(ctx context.Context, params UpdateProductModelParams) error {
-	// if isAdmin, err := s.Account.IsAdmin(ctx, params.UserID); err != nil {
-	// 	return err
-	// } else if !isAdmin {
-	// 	return model.ErrForbidden
-	// }
-
-	return s.repo.UpdateProductModel(ctx, params)
-}
-
-func (s *ProductService) DeleteProductModel(ctx context.Context, id int64) error {
-	return s.repo.DeleteProductModel(ctx, id)
-}
-
-func (s *ProductService) GetProduct(ctx context.Context, params model.ProductIdentifier) (model.Product[any], error) {
+func (s *ProductService) GetProduct(ctx context.Context, params model.ProductIdentifier) (model.Product, error) {
 	return s.repo.GetProduct(ctx, params)
 }
 
 type ListProductsParams = repository.ListProductsParams
 
-func (s *ProductService) ListProducts(ctx context.Context, params ListProductsParams) (result model.PaginateResult[model.Product[any]], err error) {
+func (s *ProductService) ListProducts(ctx context.Context, params ListProductsParams) (result model.PaginateResult[model.Product], err error) {
 	total, err := s.repo.CountProducts(ctx, params)
 	if err != nil {
 		return result, err
@@ -182,7 +36,7 @@ func (s *ProductService) ListProducts(ctx context.Context, params ListProductsPa
 		return result, err
 	}
 
-	return model.PaginateResult[model.Product[any]]{
+	return model.PaginateResult[model.Product]{
 		Data:       products,
 		Limit:      params.Limit,
 		Page:       params.Page,
@@ -192,19 +46,49 @@ func (s *ProductService) ListProducts(ctx context.Context, params ListProductsPa
 	}, nil
 }
 
-func (s *ProductService) CreateProduct(ctx context.Context, product model.Product[any]) (model.Product[any], error) {
+func (s *ProductService) CreateProduct(ctx context.Context, product model.Product) (model.Product, error) {
 	newProduct, err := s.repo.CreateProduct(ctx, product)
 	if err != nil {
-		return model.Product[any]{}, err
+		return model.Product{}, err
 	}
 
 	return newProduct, nil
 }
 
-type UpdateProductParams = repository.UpdateProductParams
+type UpdateProductParams = struct {
+	RepoParams repository.UpdateProductParams
+	Resources  []string
+}
 
 func (s *ProductService) UpdateProduct(ctx context.Context, params UpdateProductParams) error {
-	return s.repo.UpdateProduct(ctx, params)
+	txRepo, err := s.repo.Begin(ctx)
+	if err != nil {
+		return err
+	}
+	defer txRepo.Rollback(ctx)
+
+	if err = s.repo.UpdateProduct(ctx, params.RepoParams); err != nil {
+		return err
+	}
+
+	current, err := txRepo.GetResources(ctx, params.RepoParams.ID)
+	if err != nil {
+		return err
+	}
+
+	added, removed := util.Diff(current, params.Resources)
+	if err = txRepo.AddResources(ctx, params.RepoParams.ID, added); err != nil {
+		return err
+	}
+	if err = txRepo.RemoveResources(ctx, params.RepoParams.ID, removed); err != nil {
+		return err
+	}
+
+	if err = txRepo.Commit(ctx); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (s *ProductService) DeleteProduct(ctx context.Context, params model.ProductIdentifier) error {
@@ -223,16 +107,23 @@ type ProductServiceInterface interface {
 	CreateProductModel(ctx context.Context, params CreateProductModelParams) (model.ProductModel, error)
 	UpdateProductModel(ctx context.Context, params UpdateProductModelParams) error
 	DeleteProductModel(ctx context.Context, id int64) error
+	ListProductTypes(ctx context.Context, params ListProductTypesParams) ([]model.ProductType, error)
 
-	GetProduct(ctx context.Context, params model.ProductIdentifier) (model.Product[any], error)
-	ListProducts(ctx context.Context, params ListProductsParams) (model.PaginateResult[model.Product[any]], error)
-	CreateProduct(ctx context.Context, product model.Product[any]) (model.Product[any], error)
+	GetProduct(ctx context.Context, params model.ProductIdentifier) (model.Product, error)
+	ListProducts(ctx context.Context, params ListProductsParams) (model.PaginateResult[model.Product], error)
+	CreateProduct(ctx context.Context, product model.Product) (model.Product, error)
 	UpdateProduct(ctx context.Context, params UpdateProductParams) error
 	DeleteProduct(ctx context.Context, params model.ProductIdentifier) error
 
-	CreateSale(ctx context.Context, sale model.Sale) (model.Sale, error)
+	GetSale(ctx context.Context, id int64) (model.Sale, error)
+	ListSales(ctx context.Context, params ListSalesParams) (model.PaginateResult[model.Sale], error)
+	CreateSale(ctx context.Context, params CreateSaleParams) (model.Sale, error)
+	UpdateSale(ctx context.Context, params UpdateSaleParams) error
 	DeleteSale(ctx context.Context, id int64) error
 
-	CreateTag(ctx context.Context, tag model.Tag) (model.Tag, error)
-	DeleteTag(ctx context.Context, name string) error
+	GetTag(ctx context.Context, tag string) (TagResponse, error)
+	ListTags(ctx context.Context, params ListTagsParams) (model.PaginateResult[TagResponse], error)
+	CreateTag(ctx context.Context, tag model.Tag) error
+	UpdateTag(ctx context.Context, params UpdateTagParams) error
+	DeleteTag(ctx context.Context, tag string) error
 }
