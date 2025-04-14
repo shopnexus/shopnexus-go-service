6c8591687c7a3295280083a59a39a61f4efbf815
Đậu Khoa <khoakomlem@gmail.com>
2025-04-14 15:39:10 +0700

    refactor database (product, payment, refund, ..)

--- prisma/migrations/0_init/migration.sql
+++ prisma/migrations/0_init/migration.sql
@@ -11,13 +11,16 @@ CREATE SCHEMA IF NOT EXISTS "product";
 CREATE TYPE "account"."gender" AS ENUM ('MALE', 'FEMALE', 'OTHER');
 
 -- CreateEnum
+CREATE TYPE "product"."comment_type" AS ENUM ('PRODUCT_MODEL', 'BRAND', 'COMMENT');
+
+-- CreateEnum
 CREATE TYPE "payment"."payment_method" AS ENUM ('CASH', 'VNPAY', 'MOMO');
 
 -- CreateEnum
 CREATE TYPE "payment"."refund_method" AS ENUM ('PICK_UP', 'DROP_OFF');
 
 -- CreateEnum
-CREATE TYPE "payment"."status" AS ENUM ('PENDING', 'SUCCESS', 'CANCELLED', 'FAILED');
+CREATE TYPE "payment"."status" AS ENUM ('PENDING', 'SUCCESS', 'CANCELED', 'FAILED');
 
 -- CreateTable
 CREATE TABLE "account"."base" (
@@ -79,6 +82,8 @@ CREATE TABLE "account"."address" (
     "city" TEXT NOT NULL,
     "province" TEXT NOT NULL,
     "country" TEXT NOT NULL,
+    "date_created" TIMESTAMPTZ(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "date_updated" TIMESTAMPTZ(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "address_pkey" PRIMARY KEY ("id")
 );
@@ -93,10 +98,10 @@ CREATE TABLE "account"."cart" (
 -- CreateTable
 CREATE TABLE "account"."item_on_cart" (
     "cart_id" BIGINT NOT NULL,
-    "product_model_id" BIGINT NOT NULL,
+    "product_id" BIGINT NOT NULL,
     "quantity" BIGINT NOT NULL,
 
-    CONSTRAINT "item_on_cart_pkey" PRIMARY KEY ("cart_id","product_model_id")
+    CONSTRAINT "item_on_cart_pkey" PRIMARY KEY ("cart_id","product_id")
 );
 
 -- CreateTable
@@ -124,6 +129,7 @@ CREATE TABLE "product"."model" (
 -- CreateTable
 CREATE TABLE "product"."comment" (
     "id" BIGSERIAL NOT NULL,
+    "type" "product"."comment_type" NOT NULL,
     "account_id" BIGINT NOT NULL,
     "dest_id" BIGINT NOT NULL,
     "body" TEXT NOT NULL,
@@ -139,17 +145,27 @@ CREATE TABLE "product"."comment" (
 -- CreateTable
 CREATE TABLE "product"."base" (
     "id" BIGSERIAL NOT NULL,
-    "serial_id" TEXT NOT NULL,
     "product_model_id" BIGINT NOT NULL,
     "quantity" BIGINT NOT NULL DEFAULT 0,
     "sold" BIGINT NOT NULL DEFAULT 0,
     "add_price" BIGINT NOT NULL DEFAULT 0,
     "is_active" BOOLEAN NOT NULL DEFAULT true,
+    "can_combine" BOOLEAN NOT NULL DEFAULT false,
     "metadata" JSONB NOT NULL DEFAULT '{}',
     "date_created" TIMESTAMPTZ(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "date_updated" TIMESTAMPTZ(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "base_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "product"."serial" (
+    "serial_id" TEXT NOT NULL,
+    "product_id" BIGINT NOT NULL,
+    "is_sold" BOOLEAN NOT NULL DEFAULT false,
+    "is_active" BOOLEAN NOT NULL DEFAULT true,
+    "date_created" TIMESTAMPTZ(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "date_updated" TIMESTAMPTZ(3) NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 -- CreateTable
@@ -196,14 +212,23 @@ CREATE TABLE "product"."tag" (
 );
 
 -- CreateTable
-CREATE TABLE "payment"."product_on_payment" (
-    "payment_id" BIGINT NOT NULL,
+CREATE TABLE "payment"."product_serial_on_product_on_payment" (
+    "product_on_payment_id" BIGINT NOT NULL,
     "product_serial_id" TEXT NOT NULL,
+
+    CONSTRAINT "product_serial_on_product_on_payment_pkey" PRIMARY KEY ("product_on_payment_id","product_serial_id")
+);
+
+-- CreateTable
+CREATE TABLE "payment"."product_on_payment" (
+    "id" BIGSERIAL NOT NULL,
+    "payment_id" BIGINT NOT NULL,
+    "product_id" BIGINT NOT NULL,
     "quantity" BIGINT NOT NULL,
     "price" BIGINT NOT NULL,
     "total_price" BIGINT NOT NULL,
 
-    CONSTRAINT "product_on_payment_pkey" PRIMARY KEY ("payment_id","product_serial_id")
+    CONSTRAINT "product_on_payment_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -235,7 +260,7 @@ CREATE TABLE "payment"."vnpay" (
 -- CreateTable
 CREATE TABLE "payment"."refund" (
     "id" BIGSERIAL NOT NULL,
-    "payment_id" BIGINT NOT NULL,
+    "product_on_payment_id" BIGINT NOT NULL,
     "method" "payment"."refund_method" NOT NULL,
     "status" "payment"."status" NOT NULL,
     "reason" TEXT NOT NULL,
@@ -267,13 +292,16 @@ CREATE UNIQUE INDEX "user_phone_key" ON "account"."user"("phone");
 CREATE UNIQUE INDEX "comment_account_id_dest_id_key" ON "product"."comment"("account_id", "dest_id");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "base_serial_id_key" ON "product"."base"("serial_id");
-
--- CreateIndex
 CREATE INDEX "base_product_model_id_sold_idx" ON "product"."base"("product_model_id", "sold");
 
 -- CreateIndex
+CREATE UNIQUE INDEX "serial_serial_id_key" ON "product"."serial"("serial_id");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "type_name_key" ON "product"."type"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "product_on_payment_payment_id_product_id_key" ON "payment"."product_on_payment"("payment_id", "product_id");
 
 -- AddForeignKey
 ALTER TABLE "account"."base" ADD CONSTRAINT "base_role_fkey" FOREIGN KEY ("role") REFERENCES "account"."role"("name") ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -300,7 +328,7 @@ ALTER TABLE "account"."cart" ADD CONSTRAINT "cart_id_fkey" FOREIGN KEY ("id") RE
 ALTER TABLE "account"."item_on_cart" ADD CONSTRAINT "item_on_cart_cart_id_fkey" FOREIGN KEY ("cart_id") REFERENCES "account"."cart"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "account"."item_on_cart" ADD CONSTRAINT "item_on_cart_product_model_id_fkey" FOREIGN KEY ("product_model_id") REFERENCES "product"."model"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "account"."item_on_cart" ADD CONSTRAINT "item_on_cart_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "product"."base"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "product"."model" ADD CONSTRAINT "model_type_fkey" FOREIGN KEY ("type") REFERENCES "product"."type"("id") ON DELETE CASCADE ON UPDATE CASCADE;
@@ -313,6 +341,9 @@ ALTER TABLE "product"."comment" ADD CONSTRAINT "comment_account_id_fkey" FOREIGN
 
 -- AddForeignKey
 ALTER TABLE "product"."base" ADD CONSTRAINT "base_product_model_id_fkey" FOREIGN KEY ("product_model_id") REFERENCES "product"."model"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "product"."serial" ADD CONSTRAINT "serial_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "product"."base"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "product"."sale" ADD CONSTRAINT "sale_tag_fkey" FOREIGN KEY ("tag") REFERENCES "product"."tag"("tag") ON DELETE CASCADE ON UPDATE CASCADE;
@@ -330,10 +361,16 @@ ALTER TABLE "product"."tag_on_product_model" ADD CONSTRAINT "tag_on_product_mode
 ALTER TABLE "product"."tag_on_product_model" ADD CONSTRAINT "tag_on_product_model_tag_fkey" FOREIGN KEY ("tag") REFERENCES "product"."tag"("tag") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
+ALTER TABLE "payment"."product_serial_on_product_on_payment" ADD CONSTRAINT "product_serial_on_product_on_payment_product_on_payment_id_fkey" FOREIGN KEY ("product_on_payment_id") REFERENCES "payment"."product_on_payment"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "payment"."product_serial_on_product_on_payment" ADD CONSTRAINT "product_serial_on_product_on_payment_product_serial_id_fkey" FOREIGN KEY ("product_serial_id") REFERENCES "product"."serial"("serial_id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
 ALTER TABLE "payment"."product_on_payment" ADD CONSTRAINT "product_on_payment_payment_id_fkey" FOREIGN KEY ("payment_id") REFERENCES "payment"."base"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "payment"."product_on_payment" ADD CONSTRAINT "product_on_payment_product_serial_id_fkey" FOREIGN KEY ("product_serial_id") REFERENCES "product"."base"("serial_id") ON DELETE NO ACTION ON UPDATE CASCADE;
+ALTER TABLE "payment"."product_on_payment" ADD CONSTRAINT "product_on_payment_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "product"."base"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "payment"."base" ADD CONSTRAINT "base_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "account"."user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
@@ -342,5 +379,5 @@ ALTER TABLE "payment"."base" ADD CONSTRAINT "base_user_id_fkey" FOREIGN KEY ("us
 ALTER TABLE "payment"."vnpay" ADD CONSTRAINT "vnpay_id_fkey" FOREIGN KEY ("id") REFERENCES "payment"."base"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "payment"."refund" ADD CONSTRAINT "refund_payment_id_fkey" FOREIGN KEY ("payment_id") REFERENCES "payment"."base"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "payment"."refund" ADD CONSTRAINT "refund_product_on_payment_id_fkey" FOREIGN KEY ("product_on_payment_id") REFERENCES "payment"."product_on_payment"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
