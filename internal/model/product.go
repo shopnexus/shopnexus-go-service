2a1756350951ec682f95d24cf66be866c744499a
Đậu Khoa <khoakomlem@gmail.com>
2025-04-14 16:00:26 +0700

    fix product model (remove serialID, add CanCombine)

--- internal/model/product.go
+++ internal/model/product.go
@@ -2,20 +2,14 @@ package model
 
 import "encoding/json"
 
-// ProductIdentifier is a struct to identify a product, either by ID or SerialID
-type ProductIdentifier struct {
-	ID       *int64
-	SerialID *string
-}
-
 type Product struct {
-	ID             int64           `json:"id"`        /* unique */
-	SerialID       string          `json:"serial_id"` /* unique */
+	ID             int64           `json:"id"` /* unique */
+	ProductModelID int64           `json:"product_model_id"`
 	Quantity       int64           `json:"quantity,omitempty"`
 	Sold           int64           `json:"sold,omitempty"`
 	AddPrice       int64           `json:"add_price,omitempty"`
 	IsActive       bool            `json:"is_active"`
-	ProductModelID int64           `json:"product_model_id"`
+	CanCombine     bool            `json:"can_combine"`
 	Metadata       json.RawMessage `json:"metadata"`
 	DateCreated    int64           `json:"date_created"`
 	DateUpdated    int64           `json:"date_updated"`
